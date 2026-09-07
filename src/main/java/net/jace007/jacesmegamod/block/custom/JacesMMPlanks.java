3211518c48a06aca0d762156bed1648d7de8aa69
iix-Jace007 <juliaarinsberg07@gmail.com>
2026-09-07 18:52:40 -0400

    Added custom block classes

---
+++ src/main/java/net/jace007/jacesmegamod/block/custom/JacesMMPlanks.java
@@ -0,0 +1,25 @@
+package net.jace007.jacesmegamod.block.custom;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class JacesMMPlanks extends Block {
+    public JacesMMPlanks(Properties properties) {super(properties); }
+    @Override
+    public boolean isFlammable(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+        return true;
+    }
+
+    @Override
+    public int getFlammability(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+        return 20;
+    }
+
+    @Override
+    public int getFireSpreadSpeed(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+        return 5;
+    }
+}
